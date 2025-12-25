9b77fdd1ae791f94adc35e71add800b81f1f77c1
sushantg57 <sushantgaire57@gmail.com>
2025-12-25 15:53:47 +0545

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,6 @@
     <title>HotelSphere</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Optional JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
