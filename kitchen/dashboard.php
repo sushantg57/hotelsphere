ce28d18b815846cd362deb5be042a6ccabdc0ca5
sushantg57 <sushantgaire57@gmail.com>
2025-12-25 15:07:17 +0545

    Update dashboard.php

--- kitchen/dashboard.php
+++ kitchen/dashboard.php
@@ -8,4 +8,5 @@ if ($_SESSION['role_id'] != 3) {
 
 <h1>Kitchen Dashboard</h1>
 <p>Welcome, <?php echo $_SESSION['name']; ?>!</p>
-<a href="../auth/logout.php">Logout</a>
+
+<p><a href="../auth/logout.php">Logout</a></p>
